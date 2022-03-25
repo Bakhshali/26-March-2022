d9980aabf5cf26dcdd1e4e3bb5d4d4ba1677658f
Bakhshali <tu78rfrfu@code.edu.az>
2022-03-25 10:34:54 +0400

    Task 1 added

---
+++ ConsoleApp28/ConsoleApp28/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+
+namespace ConsoleApp28
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(isOdd(11));
+
+            Console.WriteLine(isEven(11));
+        }
+
+       
+        public static bool isOdd(int num1)
+        {
+            return num1 % 2 == 1;
+        }
+
+        public static bool isEven(int num1)
+        {
+            return num1 % 2 == 0;
+          
+        }
+    }
+}
+
