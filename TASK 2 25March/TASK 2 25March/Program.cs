89ce5cf5731a2b26c30a2232b8982bfedb5a24ad
Bakhshali <tu78rfrfu@code.edu.az>
2022-03-25 10:34:16 +0400

    Task 2 added

---
+++ TASK 2 25March/TASK 2 25March/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace TASK_2_25March
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string groupNo;
+            string fullname;
+            
+
+            do
+            {
+
+                Console.WriteLine("Please Enter GroupNo \n");
+
+                groupNo = Console.ReadLine();
+
+            } while (!CheckGroupNo(groupNo));
+
+            do
+            {
+                Console.WriteLine("Please Enter Fullname \n");
+
+                fullname = Console.ReadLine();
+
+            } while (!CheckFullname(fullname));
+
+            
+            
+
+        }
+
+
+
+        public static bool CheckGroupNo(string groupNo)
+        {
+            if (groupNo.Length <=
+                4)
+            {
+                bool hasDigit = false;
+                bool hasLower = false;
+
+                foreach (char no in groupNo)
+                {
+                    if (char.IsDigit(no))
+                    {
+                        hasDigit = true;
+                        continue;
+                    }
+                    if (char.IsLower(no))
+                    {
+                        hasLower = true;
+
+                    }
+                }
+                bool result = hasDigit && hasLower;
+                return result;
+            }
+            return false;
+
+        }
+
+        public static bool CheckFullname(string fullname)
+        {
+            int count = 0;
+
+            for (int i = 0; i < fullname.Length; i++)
+            {
+                if (char.IsUpper(fullname[i]) && i == 0)
+                {
+                    count++;
+                }
+                else if (char.IsWhiteSpace(fullname[i]))
+                {
+                    count++;
+
+                    if (char.IsUpper(fullname[i + 1]))
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count == 3 ? true : false;
+        }
+    }
+
+    
+    
+}
+
+
