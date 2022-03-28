4e907414cd1cb10d78967fc5854039993e75075c
Bakhshali <tu78rfrfu@code.edu.az>
2022-03-28 12:37:44 +0400

    Task 3 added

---
+++ Practice.Task3/Practice.Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Practice.Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            getFirstWords("Sir hemise shirdi");
+           
+        }
+
+        public static void getFirstWords(string sentence)
+        {
+            string result = sentence.Substring(0, sentence.IndexOf(" "));
+            Console.WriteLine(result);
+        }
+    }
+}
