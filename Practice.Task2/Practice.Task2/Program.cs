0bab8db3f7fd28fc0abeba5a5270adaa7fcdbb70
Bakhshali <tu78rfrfu@code.edu.az>
2022-03-28 12:28:35 +0400

    TASK 2 added

---
+++ Practice.Task2/Practice.Task2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Practice.Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(getValue());
+
+        }
+
+
+        public static bool getValue()
+        {
+            string sentence = "Bu1";
+
+            bool hasDigit = false;
+            bool hasUpper = false;
+            bool hasLower = false;
+
+            foreach (var item in sentence)
+            {
+
+                if (char.IsDigit(item))
+                {
+                    hasDigit = true;
+                    continue;
+                }
+
+                if (char.IsUpper(item))
+                {
+                    hasUpper = true;
+                    continue;
+                }
+
+                if (char.IsLower(item))
+                {
+                    hasLower = true;
+                    
+                }              
+            }
+            bool result = hasLower && hasDigit && hasUpper;
+
+            return result;
+        }
+    }
+}
