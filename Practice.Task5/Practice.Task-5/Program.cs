2cd8b6297b242d792435ec092f5fef71fc8d65f0
Bakhshali <tu78rfrfu@code.edu.az>
2022-03-28 12:39:14 +0400

    Task 5 added

---
+++ Practice.Task5/Practice.Task-5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Practice.Task_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] array = { 1, 2, 3, 4, 5, 6,};
+
+            getResize(ref array);
+
+            
+        }
+
+        public static void getResize(ref int [] array)
+        {
+            Array.Resize(ref array, array.Length + 1);
+            
+            array[array.Length - 1] = 65;
+           
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);    
+            }        
+           
+        }
+    }
+}
