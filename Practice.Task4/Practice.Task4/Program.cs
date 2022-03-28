9b7eb298726b5edece3019f43d44578c74a957b9
Bakhshali <tu78rfrfu@code.edu.az>
2022-03-28 12:38:47 +0400

    Task 4 added

---
+++ Practice.Task4/Practice.Task4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Practice.Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine(Check());
+
+
+
+        }
+
+        public static bool Check()
+        {
+            string words = "bu gun";
+            int count = 0;
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (char.IsUpper(words[i]))
+                {
+                    count++;
+                }
+                else if (char.IsWhiteSpace(words[i]))
+                {
+                    count++;
+                    if (char.IsUpper(words[i + 1]))
+                    {
+                        count++;
+                        return true;
+                    }
+                    if(words[i] == ' ' && i == words.Length - 1)
+                    {
+                        count++;
+                        return true;
+                    }
+                }
+            }
+            return false;
+       }
+    }
+}
